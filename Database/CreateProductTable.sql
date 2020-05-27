8d3cd5b70f5b0efb341565c2fc506f694ff78c18
Dylan Carstens <dylanca@bbd.co.za>
2020-05-27 19:03:10 +0200

    updated productImage type

--- Database/CreateProductTable.sql
+++ Database/CreateProductTable.sql
@@ -7,6 +7,6 @@ CREATE TABLE CoroNacessitiesDB.dbo.Product(
 [Price] DECIMAL(10,2) NOT NULL,
 [ProductStatusID] INT NOT NULL,
 [TypeID] INT NOT NULL,
-[ProductImage] VARCHAR(MAX),
+[ProductImage] VARBINARY(MAX),
 PRIMARY KEY ([ProductID])
 );
